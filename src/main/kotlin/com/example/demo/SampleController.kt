100d44e7e0ea7a542200556c19459339f01ee97e
kewashi1 <takeshi.kamakura111111@gmail.com>
2022-01-13 15:25:49 +0900

    first

--- src/main/kotlin/com/example/demo/SampleController.kt
+++ src/main/kotlin/com/example/demo/SampleController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class SampleController {
     @GetMapping("/")
     fun getHello() :String {
-        return "Hello World"
+        return "test Hello World"
     }
 }
